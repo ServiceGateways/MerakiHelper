11ff825470432262a06a03341f8f8f9f6de02a78
ServiceGateways <56607117+ServiceGateways@users.noreply.github.com>
2022-04-19 22:18:34 +0100

    Update MerakiHelerSupportFile.py

--- MerakiHelerSupportFile.py
+++ MerakiHelerSupportFile.py
@@ -870,7 +870,7 @@ def DeviceUp(uplinks,serial,name,id):
 	for interfaces in uplinks:
 		if interfaces.get('status') != "active":
 			DeviceStatus=False
-			LoggingAddUplinks(serial,"***DEVICE OFFLINE***", " ", name, id)
+			LoggingAddUplinks(serial,"***DEVICE WARNING***", " ", name, id)
 			return(DeviceStatus)
 	return(DeviceStatus)
 ##############################################################################################		
