fd2dd2a996e86f7be3e41041c68241d4f4e5cffe
ServiceGateways <56607117+ServiceGateways@users.noreply.github.com>
2022-04-26 16:52:56 +0100

    Update MerakiHelerSupportFile.py

--- MerakiHelerSupportFile.py
+++ MerakiHelerSupportFile.py
@@ -336,13 +336,13 @@ def BigLoop(RWmode, OrgResponse, FixOrg):
 			if RWmode == False:
 				LoggingAdd("Org admins: failed", "Err", Orgs.get('name'),Orgs.get('id'))		
 			if RWmode == True:	
-				CreateAdminResponse = CreateAdmin(Orgs.get('id'), CustomerRWname, CustomerRWacess)				
+				CreateAdminResponse = CreateAdmin(Orgs.get('id'), eval(CustomerRWname), eval(CustomerRWacess))				
 		if FoundCustomerRoAdmin == False:
 			#create role for Meraki RO admin
 			if RWmode == False:
 				LoggingAdd("Org admins: failed", "Err", Orgs.get('name'),Orgs.get('id'))		
 			if RWmode == True:
-				CreateAdminResponse = CreateAdmin(OrgID = Orgs.get('id'), CustomerROname, CustomerROaccess)
+				CreateAdminResponse = CreateAdmin(OrgID = Orgs.get('id'), eval(CustomerROname), eval(CustomerROaccess))
 	
 ##############################################################################################	
 # *** BIG DELETE FUNCTIONS ***
