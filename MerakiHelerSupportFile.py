1b93a7e160ca27c18a31cda1da40e36b28df96eb
ServiceGateways <56607117+ServiceGateways@users.noreply.github.com>
2022-04-27 14:19:36 +0100

    Update MerakiHelerSupportFile.py

--- MerakiHelerSupportFile.py
+++ MerakiHelerSupportFile.py
@@ -676,12 +676,11 @@ def CheckLoss(OrgResponse):
 		for Interfaces in InterfacesStats:
 			TimeSeries = Interfaces.get('timeSeries')
 			for statistics in TimeSeries:
-				print(statistics.get('lossPercent'))
 				if float(statistics.get('lossPercent')) > 5 or str(statistics.get('lossPercent')) == "0.0": 
 					CompressedDesc = (Orgs.get('serial') + " " + Orgs.get('uplink'))
 					CompressedStatus = ( "Err loss = " + TimeSeries.get('lossPercent'))
 					LoggingAdd(CompressedDesc , CompressedStatus , Orgs.get('name'), Orgs.get('id'))
-				print("hi")	
+
 ##############################################################################################
 # End of Functions
 ##############################################################################################
