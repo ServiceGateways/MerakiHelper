657c48252501a0c61b5bde8f6b5f6e72974915ed
ServiceGateways <56607117+ServiceGateways@users.noreply.github.com>
2022-12-04 15:14:42 +0000

    Update MerakiHelerSupportFile.py

--- MerakiHelerSupportFile.py
+++ MerakiHelerSupportFile.py
@@ -608,6 +608,7 @@ def parseArguments():
 	parser.add_argument("--loss", help="List MX Appliances with WAN interface issues", action="store_true")
 	parser.add_argument("--review", help="Compliance check for ops", action="store_true")
 	parser.add_argument("--down", help="List devices which are down", action="store_true")
+	parser.add_argument("--lic", help="List lics", action="store_true")
 	parser.add_argument("--version", action="version", version='%(prog)s - Version 2.9')
     #parser.add_argument("--api", help="Plain text API", type=str)
     #parser.add_argument("--usr", help="Plain text usrname", type=str)
